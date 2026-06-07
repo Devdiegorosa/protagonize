ea7f13a87137ffbccbcd4c20ad91b5e3fc630a77
DIEGO ROSA <dev.diegorosa@gmail.com>
2026-06-06 21:13:59 -0400

    Configure GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/protagonize',
   images: {
-    remotePatterns: [],
+    unoptimized: true,
   },
 }
+
 export default nextConfig
