bbf4e4b47c36b00abbcaaa8cfaa5bd236a76395a
DIEGO ROSA <dev.diegorosa@gmail.com>
2026-06-06 21:31:11 -0400

    Atualizei o next.conf.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
+/ ** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/protagonize',
   images: {
     unoptimized: true,
   },
